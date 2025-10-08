0fdd1a4caa58ab42f6974480a7a2f0ee04401e96
Suman Kagithala <kagithalasuman1030@gmail.com>
2025-10-08 15:47:10 +0530

    link changed

--- src/Foodhub/Pages/Link.jsx
+++ src/Foodhub/Pages/Link.jsx
@@ -4,4 +4,5 @@
 export const key_id='rzp_test_v1sxGn5PiHxGVH'  
 
 
-export const Api_Path="https://backend-nodejs-foodhub-1.onrender.com"
+//export const Api_Path="https://backend-nodejs-foodhub-1.onrender.com"
+export const Api_Path="https://foodhub-backend-hmv6utam4q-el.a.run.app/"
--- src/Foodhub/Pages/Redis.js
+++
@@ -1,8 +0,0 @@
-const redis = require('redis')
-
-const redisClient=redis.createClient();
-
-redisClient.connect()
-
-
-module.exports=redisClient;
