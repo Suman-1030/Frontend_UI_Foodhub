617915e38dcf130e9fd3c37294fcc54709225d55
Suman Kagithala <kagithalasuman1030@gmail.com>
2025-10-26 10:31:30 +0530

    backend link updated

--- src/Foodhub/Pages/Link.jsx
+++ src/Foodhub/Pages/Link.jsx
@@ -1,8 +1,9 @@
 
-//export const Api_Path='http://localhost:4000'
+//export const Api_Path='http://localhost:8080'
 
 export const key_id='rzp_test_v1sxGn5PiHxGVH'  
 
 
 //export const Api_Path="https://backend-nodejs-foodhub-1.onrender.com"
-export const Api_Path="https://foodhub-backend-hmv6utam4q-el.a.run.app/"
+
+export const Api_Path="https://foodhub-backend-service-hmv6utam4q-el.a.run.app"
